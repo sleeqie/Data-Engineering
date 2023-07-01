0ccaf0663a34cac898df8c8d9832a1d046c00dab
Adedamola Yusuf <adedamolayusuf@MacBook-Pro.lan>
2023-07-01 21:00:14 +0200

    kafka

---
+++ Kafka/consumer.py
@@ -0,0 +1,22 @@
+from confluent_kafka import Consumer
+
+c = Consumer({
+    'bootstrap.servers': 'localhost:9092,localhost:9093,localhost:9093',
+    'group.id': 'python-consumer',
+    'auto.offset.reset': 'earliest'
+})
+
+c.subscribe(['users'])
+
+while True:
+    msg = c.poll(1.0)  # Timeout
+    if msg is None:
+        continue
+    if msg.error():
+        print('Error: {}'.format(msg.error()))
+        continue
+    data = msg.value().decode('utf-8')
+    print(data)
+
+c.close()
+
